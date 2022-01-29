b49837f1b64d2d13739e8dfaf63c24220480cc4b
Balaji59321 <Balaji59321@gmail.com>
2022-01-29 22:03:22 +0530

    Add files via upload

---
+++ class17/index.js
@@ -0,0 +1,37 @@
+setTimeout(() => {
+  document.getElementsByClassName("p")[0].innerHTML = 10;
+  setTimeout(() => {
+    document.getElementsByClassName("p")[0].innerHTML = 9;
+    setTimeout(() => {
+      document.getElementsByClassName("p")[0].innerHTML = 8;
+      setTimeout(() => {
+        document.getElementsByClassName("p")[0].innerHTML = 7;
+        setTimeout(() => {
+          document.getElementsByClassName("p")[0].innerHTML = 6;
+          setTimeout(() => {
+            document.getElementsByClassName("p")[0].innerHTML = 5;
+            setTimeout(() => {
+              document.getElementsByClassName("p")[0].innerHTML = 4;
+              setTimeout(() => {
+                document.getElementsByClassName("p")[0].innerHTML = 3;
+                setTimeout(() => {
+                  document.getElementsByClassName("p")[0].innerHTML = 2;
+                  setTimeout(() => {
+                    document.getElementsByClassName("p")[0].innerHTML = 1;
+                    setTimeout(() => {
+                      document.getElementsByClassName("p")[0].innerHTML = 0;
+                      setTimeout(() => {
+                        document.getElementsByClassName("p")[0].innerHTML =
+                          "Happy Independance Day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
